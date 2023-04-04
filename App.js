13d86f51158a1392f14bc18043ee12a08131dae0
Marc Mendoza <777marc@gmail.com>
2023-04-03 20:11:07 -0400

    init

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import PizzaTranslator from './components/pizza';
+import LeCat from './components/LeCat';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!?</Text>
       <PizzaTranslator />
+      {/* <LeCat /> */}
       <StatusBar style="auto" />
     </View>
   );
